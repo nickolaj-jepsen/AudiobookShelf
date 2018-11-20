b6bfac557a984619d085aed13924734f58735154
Nickolaj Jepsen <nickolaj1177@gmail.com>
2018-11-20 01:12:49 +0100

    fix attribution

--- types/podcast/index.d.ts
+++ types/podcast/index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for podcast v0.1.0
 // Project: https://github.com/maxnowack/node-podcast
-// Definitions by: Niklas Mollenhauer <https://github.com/nikeee>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Definitions originally by: Niklas Mollenhauer <https://github.com/nikeee>
+// Definitions based on: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 interface IFeedOptions {
     title: string;
